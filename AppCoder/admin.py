8985844d1d5b5efeead310d29d2b66beac449eca
vanerm <vanesarmizrahi@gmail.com>
2025-07-10 09:41:11 -0300

    Update admin: register Profesor and Alumno models

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *   # Import all models from the current app
 
 admin.site.register(Curso)
+admin.site.register(Alumno)
+admin.site.register(Profesor)
 
 # Register your models here.
